f7c8723cd641932706d8e0a7701b1194a2f21e09
S-H-GAMELINKS <gamelinks007@gmail.com>
2023-03-08 23:32:20 +0900

    Fix main.rb

--- main.rb
+++ main.rb
@@ -1,14 +1,30 @@
 require 'dotenv'
 require 'json'
 require 'mastodon'
+require 'clockwork'
+require 'logger'
 
+include Clockwork
+
+# load .env
 Dotenv.load
 
-client = Mastodon::REST::Client.new(base_url: ENV["MASTODON_URL"], bearer_token: ENV["MASTODON_ACCESS_TOKEN"])
+# generate logger
+logger = Logger.new(STDERR)
 
-client.hashtag_timeline("顔面工事").each do |toot|
+# handler for job
+handler do |job|
+  puts "Running #{job}"
+end
+
+# Running job with every 30 minitues
+every(30.minutes, 'boost.job') do 
+  client = Mastodon::REST::Client.new(base_url: ENV["MASTODON_URL"], bearer_token: ENV["MASTODON_ACCESS_TOKEN"])
+
+  client.hashtag_timeline("顔面工事").each do |toot|
     if !toot.reblogged? then
       response = client.reblog(toot.id)
       response = client.favourite(toot.id)
     end
+  end
 end
